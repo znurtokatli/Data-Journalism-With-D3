e4c84c1aa37df3fc8500a0ed4ac7bd5291fb7502
znurtokatli <50844314+znurtokatli@users.noreply.github.com>
2021-04-19 12:29:29 -0400

    svg

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,24 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 600;
+
+var margin = {
+  top: 20,
+  right: 40,
+  left: 60,
+  bottom: 100
+};
+
+// calculate chart height and width
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+ 
+//append an svg element to the chart 
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight); 
+
+//append an svg group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+ 
