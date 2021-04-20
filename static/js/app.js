87072723f34747476867a04db94ad56e9340490f
znurtokatli <50844314+znurtokatli@users.noreply.github.com>
2021-04-19 22:07:38 -0400

    retrieve data

--- static/js/app.js
+++ static/js/app.js
@@ -20,5 +20,84 @@ var svg = d3.select("#scatter")
 
 //append an svg group
 var chartGroup = svg.append("g")
-  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+                    .attr("transform", `translate(${margin.left}, ${margin.top})`);
  
+// x and y 
+var chosenAxisX = 'poverty';
+var chosenAxisY = 'healthcare';
+
+//function for x axis selection 
+function scaleX(data, axisX) {
+    
+    var scaleLinearX = d3.scaleLinear()
+      .domain([d3.min(data, d => d[axisX]) * 0.5,
+               d3.max(data, d => d[axisX]) * 1.5])
+      .range([0, width]);
+
+    return scaleLinearX;
+}
+
+//function for x axis selection 
+function scaleY(data, axisY) {
+    
+    var scaleLinearY = d3.scaleLinear()
+      .domain([d3.min(data, d => d[axisY]) * 0.5,
+               d3.max(data, d => d[axisY]) * 1.5])
+      .range([height, 0]);
+  
+    return scaleLinearY;
+}
+
+// update x axis
+function featureAxisX(newScaleX, axisX) {
+
+    var axisBottom = d3.axisBottom(newScaleX);
+  
+    axisX.transition()
+         .duration(1000)
+         .call(axisBottom);
+  
+    return axisX;
+}
+  
+// update y axis
+function featureAxisY(newScaleY, axisY) {
+
+  var axisLeft = d3.axisLeft(newScaleY);
+
+  axisY.transition()
+       .duration(1000)
+       .call(axisLeft);
+
+  return axisY;
+}
+
+
+
+
+
+
+
+
+
+
+
+//retrieve data
+d3.csv('../data/data.csv').then(function(chartData) {
+
+  console.log(chartData);
+
+  chartData.forEach(function(data) {
+
+    data.obesity = +data.obesity;
+    data.income = +data.income;
+    data.smokes = +data.smokes;
+    data.age = +data.age;
+    data.healthcare = +data.healthcare;
+    data.poverty = +data.poverty;
+
+  });
+
+
+  
+}
